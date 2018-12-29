4219a8e4e0f5b84d3a41987f3e40fac71cbf49b9
Michael Lam <Michael Lam@192.168.0.11>
2018-12-29 15:39:44 -0500

    asd

--- Java Game Development/src/ca/jam/gameobject/Floor.java
+++ Java Game Development/src/ca/jam/gameobject/Floor.java
@@ -1,5 +1,30 @@
 package ca.jam.gameobject;
 
-public class Floor {
-	//my floor +1
+import java.awt.Color;
+import java.awt.Graphics2D;
+
+public class Floor extends GameObject{
+
+	public Floor(float x, float y, int width, int height) {
+		super(x, y);
+		this.width = width;
+		this.height = height;
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void tick() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void render(Graphics2D g) {
+		// TODO Auto-generated method stub
+		g.setColor(Color.magenta);
+		g.fillRect((int) x, (int) y, width, height);
+	}
+	
+	
+	
 }
