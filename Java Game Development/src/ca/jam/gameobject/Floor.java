84b5db591bd32a9142bd0e96de0e298a55f1db22
Jackie <jtran2009a@gmail.com>
2018-12-30 17:16:24 -0500

    Buggy Collision

--- Java Game Development/src/ca/jam/gameobject/Floor.java
+++ Java Game Development/src/ca/jam/gameobject/Floor.java
@@ -3,10 +3,10 @@ package ca.jam.gameobject;
 import java.awt.Color;
 import java.awt.Graphics2D;
 
-public class Floor extends GameObject{
+public class Floor extends GameObject {
 
 	private Player player;
-	
+
 	public Floor(float x, float y, int width, int height, Player player) {
 		super(x, y);
 		this.width = width;
@@ -17,27 +17,35 @@ public class Floor extends GameObject{
 	@Override
 	public void tick() {
 		collisionWithPlayer(player);
-		
+
 	}
-	//hi
+
+	// hi
 	@Override
 	public void render(Graphics2D g) {
 		// TODO Auto-generated method stub
 		g.setColor(Color.magenta);
 		g.fillRect((int) x, (int) y, width, height);
 	}
-	
+
 	public void collisionWithPlayer(Player player) {
-		//Player lands ontop of the floor
-		if (player.getX() >= x && player.getX() + player.getWidth() <= x + width && player.getVelY() > 0) {
-			if (player.getY() + player.getHeight() >= y ) {
-				player.setY(y - player.getHeight() - player.getVelY());
-				player.setGrounded(true);
-			} else {
-				player.setGrounded(false);
+		// Possibly above or below the floor
+		if (player.getX() + player.getWidth() >= x && player.getX() <= x + width) {
+			// falling down, therefore landing on the floor
+			if (player.getVelY() > 0) {
+				if (player.getY() + player.getHeight() > y && player.getY() + player.getHeight() < y + height) {
+					player.setGrounded(true);
+					player.setVelY(0);
+					player.setY(y - player.getHeight());
+				}
 			}
+		} else {
+			player.setGrounded(false);
 		}
 	}
-	
-	
+	/*
+	 * else { if (player.getY() < y + height && player.getY() > y) { player.setY(y +
+	 * height); player.setVelY(5); } }
+	 */
+
 }
